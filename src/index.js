bddf946358dc69336bf8a7bf144c187677fb11a6
Jared Lunde <jared.lunde@gmail.com>
2018-10-24 11:46:46 -0600

    woes + annoyances... trying to get 16.6.0 to work

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-console */
-
 // Inspired by the awesome work by the Apollo team: 😘
 // https://github.com/apollographql/react-apollo/blob/master/src/getDataFromTree.ts
 //
@@ -9,10 +7,42 @@ const defaultOptions = {
   componentWillUnmount: false,
 }
 
-const forwardRefSymbol = Symbol.for('react.forward_ref')
+const hasSymbol = typeof Symbol === 'function' && Symbol.for;
 
-// lifted from React Fiber
-const MAYBE_ITERATOR_SYMBOL = typeof Symbol === 'function' && Symbol.iterator
+export const REACT_ELEMENT_TYPE = hasSymbol
+  ? Symbol.for('react.element')
+  : 0xeac7;
+export const REACT_PORTAL_TYPE = hasSymbol
+  ? Symbol.for('react.portal')
+  : 0xeaca;
+export const REACT_FRAGMENT_TYPE = hasSymbol
+  ? Symbol.for('react.fragment')
+  : 0xeacb;
+export const REACT_STRICT_MODE_TYPE = hasSymbol
+  ? Symbol.for('react.strict_mode')
+  : 0xeacc;
+export const REACT_PROFILER_TYPE = hasSymbol
+  ? Symbol.for('react.profiler')
+  : 0xead2;
+export const REACT_PROVIDER_TYPE = hasSymbol
+  ? Symbol.for('react.provider')
+  : 0xeacd;
+export const REACT_CONTEXT_TYPE = hasSymbol
+  ? Symbol.for('react.context')
+  : 0xeace;
+export const REACT_CONCURRENT_MODE_TYPE = hasSymbol
+  ? Symbol.for('react.concurrent_mode')
+  : 0xeacf;
+export const REACT_FORWARD_REF_TYPE = hasSymbol
+  ? Symbol.for('react.forward_ref')
+  : 0xead0;
+export const REACT_SUSPENSE_TYPE = hasSymbol
+  ? Symbol.for('react.suspense')
+  : 0xead1;
+export const REACT_MEMO_TYPE = hasSymbol ? Symbol.for('react.memo') : 0xead3;
+export const REACT_LAZY_TYPE = hasSymbol ? Symbol.for('react.lazy') : 0xead4;
+
+const MAYBE_ITERATOR_SYMBOL = typeof Symbol === 'function' && Symbol.iterator;
 const FAUX_ITERATOR_SYMBOL = '@@iterator'
 
 function isIterator (maybeIterable) {
@@ -55,8 +85,11 @@ const isClassComponent = Comp =>
     Comp.prototype.isPureReactComponent !== void 0)
 
 const isForwardRef = Comp =>
-  Comp.type !== void 0 && Comp.type.$$typeof === forwardRefSymbol
-
+  Comp.type !== void 0 && Comp.type.$$typeof === REACT_FORWARD_REF_TYPE
+const isContext = Comp =>
+  Comp.type !== void 0 && Comp.type.$$typeof === REACT_CONTEXT_TYPE
+const isProvider = Comp =>
+  Comp.type !== void 0 && Comp.type.$$typeof === REACT_PROVIDER_TYPE
 
 // Recurse a React Element tree, running the provided visitor against each element.
 // If a visitor call returns `false` then we will not recurse into the respective
@@ -97,17 +130,21 @@ export default function reactTreeWalker(tree, visitor, context, options = defaul
       }
 
       if (currentElement.type !== void 0 && currentElement.type !== null) {
-        if (currentElement.type._context) {
+        if (isProvider(currentElement)) {
           // eslint-disable-next-line no-param-reassign
           currentElement.type._context._currentValue =
             currentElement.props.value
         }
-        if (currentElement.type.Provider && currentElement.type.Consumer) {
-          const el = currentElement.props.children(
-            currentElement.type.Provider._context._currentValue,
-          )
 
-          return recursive(el, currentContext)
+        if (isContext(currentElement)) {
+          return recursive(
+            currentElement.props.children(
+              currentElement.type._context !== void 0
+                ? currentElement.type._context._currentValue
+                : currentElement.type.Provider._context._currentValue,
+            ),
+            currentContext
+          )
         }
       }
 
